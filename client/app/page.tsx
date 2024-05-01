174a416cd3d929c6db3909bb053640d843892540
unverifiedchaos5017 <saifkhan501721@gmail.com>
2024-05-01 16:48:08 +0530

    made it fancy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Image from "next/image";
+import React, { useRef, useEffect } from 'react';
 import Link from "next/link";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import MyNav from "./components/Navbar";
 import Footer from "./components/Footer";
 import Card from "./components/cards/newProperty";
@@ -10,14 +15,93 @@ import RecentlyAdded from "./components/sections/RecentlyAdded";
 
 export default function Home() {
 
+  useEffect(() => {
+    const targets = document.querySelectorAll(".logo-container img");
+    const numberOfTargets = targets.length;
+    const duration = 0.7;
+    const pause = 0.75; // change this
+    const stagger = duration + pause;
+    const repeatDelay = stagger * (numberOfTargets - 1) + pause;
+
+    // Postion all the elments just outside of teh container and set their scale to 0
+    gsap.set(targets, {
+      xPercent: 200,
+      scale: 0
+    });
+
+    // Timeline with default duration
+    const tl = gsap.timeline({
+      defaults: {
+        duration: duration
+      }
+    });
+
+    // Animation #1
+    tl.to(targets, {
+      xPercent: "-=100", // move from it's current postion 1 postion over
+      scale: 1,
+      transformOrigin: "center left", // set the scale  to come from from the correct side 
+      stagger: {
+        each: stagger,
+        repeat: -1,
+        repeatDelay: repeatDelay
+      }
+    });
+
+    // Animation #2
+    tl.to(
+      targets,
+      {
+        xPercent: "-=100", // move from it's current postion 1 postion over
+        stagger: {
+          each: stagger,
+          repeat: -1,
+          repeatDelay: repeatDelay
+        }
+      },
+      stagger // start when the first has animated
+    );
+
+    // Animation #3
+    tl.to(
+      targets,
+      {
+        xPercent: "-=100", // move from it's current postion 1 postion over
+
+        stagger: {
+          each: stagger,
+          repeat: -1,
+          repeatDelay: repeatDelay
+        }
+      },
+      stagger * 2 // start when the second has animated
+    );
+
+    // Animation #4
+    tl.to(
+      targets,
+      {
+        xPercent: "-=100", // move from it's current postion 1 postion over
+        transformOrigin: "center right", // change the scale  to come from from the correct side 
+        scale: 0,
+        stagger: {
+          each: stagger,
+          repeat: -1,
+          repeatDelay: repeatDelay
+        }
+      },
+      stagger * 3 // start when the third has animated
+    );
+  }, [])
 
   return (
     <div>
       <MyNav />
       <section>
+
         <div className="grid grid-cols-1 lg:grid-cols-2">
           <div className="mx-4 lg:mx-auto py-0 lg:py-20 px-2 lg:px-44">
-            <Typography variant="h1" className="text-blue text-4xl md:text-6xl lg:text-8xl font-bold font-sans">Property<br />Verse</Typography>
+            <Typography variant="h1" className="text-blue text-4xl md:text-6xl lg:text-8xl font-bold font-sans">Property<br /><span className="text-green-600">Verse</span></Typography>
             <Typography className="mt-4 ml-2 mb-10 text-lg tracking-tighter">Welcome to a redefinition of property management. Discover seamless solutions for property owners and tenants alike.</Typography>
             <button type="button" className="focus:outline-none text-white hover:text-blue bg-blue hover:bg-transparent hover:ring-1 hover:ring-blue font-medium rounded-xl text-sm px-5 py-2.5 ">Explore Properties</button>
           </div>
@@ -35,28 +119,36 @@ export default function Home() {
       </section>
       <br />
       <section className="my-10 ">
-        <h1 className="text-4xl text-gray-700 font-bold font-roboto text-center">Our Partners</h1>
-        <div className="max-w-screen-xl mx-auto mt-12 mb-24 grid grid-cols-3  gap-4 md:grid-cols-4 lg:grid-cols-7">
-          <div className="mx-auto w-10 md:w-16 lg:w-24">
+        <div className="relative flex py-5 max-w-screen-xl mx-auto items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <h1 className="text-4xl text-gray-500 font-bold font-roboto text-center mx-8">Our Partners</h1>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+
+        <div className="logo-container max-w-screen-lg mx-auto mt-12 mb-24 flex">
+          <div className=" mx-auto logo w-10 md:w-16 lg:w-24 ">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/altdrx.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/alyf.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/assetmonk.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/brik-itt.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/hbits.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/propertyshare.jpg" />
           </div>
-          <div className=" mx-auto w-10 md:w-16 lg:w-24">
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
             <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/strata.jpg" />
+          </div>
+          <div className=" mx-auto w-12 md:w-16 lg:w-24">
+            <Image alt="logo" className="mx-auto" layout="responsive" height={100} width={100} src="/logos/ryzer.jpg" />
           </div>
         </div>
       </section>
@@ -106,6 +198,7 @@ export default function Home() {
                 <Image alt="cubes" width={100} height={100} src='/cubes.png' />
               </div>
             </div>
+
 
           </div>
         </div>
