2894e2c4d5aa6fb5d46836542f19a0b293b0a1f2
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2024-05-07 23:58:26 +0530

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -99,7 +99,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta name="google-adsense-account" content="ca-pub-8927606268275562" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8927606268275562"
+     crossorigin="anonymous"></script>
       </Head>
       <MyNav />
       <section>
