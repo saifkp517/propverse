57219e89fd501385d8fb2ad7d1028baa4bb9d5b2
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2024-05-16 17:45:13 +0530

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from 'next/head'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
   title: "PropertyVerse",
   description: "Welcome to a redefinition of property management. Discover seamless solutions for property owners and tenants alike.",
   icons: {
-    icon: 'https://i.postimg.cc/cL8MPmGw/logo-final-blue-and-green-transparent-1-1.png'
+    icon: 'https://i.postimg.cc/cL8MPmGw/logo-final-blueTheme-and-green-transparent-1-1.png'
   }
 };
 
@@ -18,8 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8927606268275562"
+          crossOrigin="anonymous"
+        ></script>
+        <meta name="google-adsense-account" content="ca-pub-8927606268275562"></meta>
+      </Head>
       <body className={inter.className}>{children}</body>
-    </html>
+    </>
   );
 }
