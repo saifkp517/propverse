bc5296a8b133e11bbded2f581ea81533cef67fdd
unverifiedchaos5017 <saifkhan501721@gmail.com>
2024-06-21 08:13:43 +0530

    update tsconfig

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -122,7 +122,7 @@ const MobileVerification = () => {
                             <input
                                 key={index}
                                 type="text"
-                                ref={(el: HTMLInputElement | null) => inputsRef.current[index] = el}
+                                ref={(el: HTMLInputElement | null) => inputsRef.current[index] = el as HTMLInputElement}
                                 className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-slate-100 border border-gray-400 hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
                                 pattern="\d*"
                                 maxLength={1}
